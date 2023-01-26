aaa52949d23962aa7f4ad34438b0723759350f37
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2023-01-26 10:12:07 +0100

    make dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+  output: "server",
+  adapter: vercel()
 });
